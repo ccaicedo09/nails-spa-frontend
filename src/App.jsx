a9590cd3700f229779eeff6778fa85aab86dbf8d
ccaicedo09 <u20231213568@usco.edu.co>
2025-02-20 16:06:42 -0500

    fix spectre import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'spectre.css/dist/spectre.min.css'
+
+import 'spectre.css/dist/spectre-icons.min.css'
+import 'spectre.css/dist/spectre-exp.min.css'
 
 function App() {
   const [count, setCount] = useState(0)
