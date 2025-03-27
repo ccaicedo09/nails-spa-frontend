295957c06db396ae0ad142dc4750384ca3d121da
Gabriel Fajardo @ Gabs <gabo2023brazil@gmail.com>
2025-03-27 16:13:56 -0500

    continue with main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,17 +11,26 @@ import Carrousel from '../components/Carrousel'
 import ImageComparision from '../components/ImageComparision'
 import Modals from '../components/Modals'
 
+import image20 from '/img/test_img_20.png'
+import image19 from '/img/test_img_19.png'
+import image18 from '/img/test_img_18.png'
+import image17 from '/img/test_img_17.png'
+import image16 from '/img/test_img_16.png'
+
+
+
+
 const Home = () => {
   return (
     <>
     <Carrousel />
-      <section className='mx-10 '>
+      <section className='mx-30 '>
       
       <Hero />
       <ImageComparision />
 
 
-      <section className='text-center'>
+      {/* <section className='text-center'>
         <div>
           <a href="https://vite.dev" target="_blank">
             <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -37,20 +46,45 @@ const Home = () => {
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
-      </section>
-    </section><section className='bg-secondary col-12'>
+      </section> */}
+    </section>
+    <section className='bg-secondary col-12 px-30 my-10'>
 
 
 
-        <div style={{ height: '100px' }} className='text-center text-primary'>
-          <h1>
-            Colombia vs brasil
-          </h1>
+        <div  className='text-center text-primary'>
+          <div className="container">
+              <div className="columns">
+                <div className="column col-6" >
+                  <div className="columns">
+                    <div className="column col-6" >
+                      <img src={image17} alt='' style={{height: '120px'}}  />
+                    </div>
+                    <div className="column col-6" >
+                    <img src={image16} alt='' style={{height: '120px'}}  />
+                    </div>
+                  </div>
+
+                  <div className="columns">
+                    <div className="column col-6" >
+                      <img src={image20} alt='' style={{height: '120px'}}  />
+                    </div>
+                    <div className="column col-6" >
+                    <img src={image18} alt='' style={{height: '120px'}}  />
+                    </div>
+                  </div>
+                </div>
+                <div className="column col-6" >col-6</div>
+              </div>
+          </div>
         </div>
 
+        
 
 
-      </section><section id='gabo' className='mx-10'>
+
+      </section>
+      <section id='gabo' className='mx-30'>
         <Modals />
         <GithubCard />
       </section>
