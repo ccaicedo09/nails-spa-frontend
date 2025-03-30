f2bbeb34abaea7633e4325c91518838fb310f4b6
unknown <u20231213568@usco.edu.co>
2025-03-30 17:30:30 -0500

    fix: export default interface -> export interface in appointment types

--- src/types/FormTypes.interface.ts
+++ src/types/FormTypes.interface.ts
@@ -1,7 +1,14 @@
-export default interface Service {
+export interface Service {
   id: number;
   name: string;
   description: string;
   estimated_duration: number;
   price: number;
+}
+
+export interface Specialist {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
 }
