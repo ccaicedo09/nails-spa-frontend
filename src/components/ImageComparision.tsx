b7c253606c121d641f20d0e7d01aa87a0f0146ab
Nathalia Avila <nathyavila05@gmail.com>
2025-05-19 09:49:12 -0500

    fix minor typos

--- src/components/ImageComparision.tsx
+++ src/components/ImageComparision.tsx
@@ -16,7 +16,7 @@ const ImageComparision = () => {
 
         <figure className="comparison-after">
             <img src="https://plus.unsplash.com/premium_photo-1670348051093-a3f94b408bcb?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            <div className="comparison-label">Despues</div>
+            <div className="comparison-label">Después</div>
             <textarea className="comparison-resizer" readOnly></textarea>
         </figure>
         </div>
