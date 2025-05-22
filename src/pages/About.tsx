f7df420d2cf1423e55eb951a8260e1739d356340
unknown <u20231213568@usco.edu.co>
2025-05-22 14:40:30 -0500

    asodfnasfas

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,24 +6,30 @@ import './styles/About.css';
 
 const teamWork = [
   {
-    name: 'Esperanza Gomez',
-    message: 'Aquí en nuestras instalaciones encontrarás un espacio diseñado para brindarte bienestar, belleza y comodidad. Somos un equipo apasionado por el detalle y la perfección, comprometidos en ofrecerte una experiencia única en cada visita.',
-    pathProfile: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
-    role: 'Diseñadora'
+    name: "Esperanza Gomez",
+    message:
+      "Aquí en nuestras instalaciones encontrarás un espacio diseñado para brindarte bienestar, belleza y comodidad. Somos un equipo apasionado por el detalle y la perfección, comprometidos en ofrecerte una experiencia única en cada visita.",
+    pathProfile:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSysEKIZbnTEierYAUQLE_looQYbmWrhQ2lag&s",
+    role: "Diseñadora",
   },
   {
-    name: 'Mia Khalifa',
-    message: 'Nos especializamos en ofrecerte servicios de la más alta calidad, con técnicas innovadoras y un equipo de profesionales que cuidan cada detalle. Queremos que cada cliente se sienta especial y disfrute de una experiencia premium.',
-    pathProfile: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
-    role: 'Modelo'
+    name: "Mia Khalifa",
+    message:
+      "Nos especializamos en ofrecerte servicios de la más alta calidad, con técnicas innovadoras y un equipo de profesionales que cuidan cada detalle. Queremos que cada cliente se sienta especial y disfrute de una experiencia premium.",
+    pathProfile:
+      "https://m.media-amazon.com/images/M/MV5BZjA0MDgyYmItNzkzMC00OTM2LThlYzktMWMxZWU3ZGNkNDI3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    role: "Modelo",
   },
   {
-    name: 'Lana Rhoades',
-    message: 'En nuestro espacio encontrarás el equilibrio perfecto entre elegancia y bienestar. Nos enfocamos en brindarte un servicio impecable, con un ambiente acogedor donde cada cliente se siente valorado y atendido con profesionalismo.',
-    pathProfile: 'https://as1.ftcdn.net/v2/jpg/03/53/11/00/1000_F_353110097_nbpmfn9iHlxef4EDIhXB1tdTD0lcWhG9.jpg',
-    role: 'Publicista'
-  }
-]
+    name: "Lana Rhoades",
+    message:
+      "En nuestro espacio encontrarás el equilibrio perfecto entre elegancia y bienestar. Nos enfocamos en brindarte un servicio impecable, con un ambiente acogedor donde cada cliente se siente valorado y atendido con profesionalismo.",
+    pathProfile:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b4/Lana_Rhoades_2-2017_%28cropped%29.jpg",
+    role: "Publicista",
+  },
+];
 
 const About = () => {
   return (
