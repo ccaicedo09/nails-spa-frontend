8e5e93e43c67cec784944afe4a6465b341d1135c
Julian Enrique <julianbuitragocharry2006@gmail.com>
2025-10-29 19:10:39 -0500

    add auth and recovery api and updated types

---
+++ src/api/auth.ts
@@ -0,0 +1,20 @@
+import { User } from "../types/usuarios";
+import { Auth } from "../types/auth"
+import { ResponseAuth } from "../types/responseAuth";
+import instance from "./axios";
+
+export const createCustomer = (customer: Omit<User, '_id' | 'role' | 'locations'>) => {
+  return instance.post<ResponseAuth>("/auth/register", customer);
+}
+
+export const accessAccount = (account: Auth) => {
+  return instance.post<ResponseAuth>("/auth/login", account);
+}
+
+export const logoutAccount = () => {
+  return instance.get<ResponseAuth>("/auth/logout");
+}
+
+export const checkCredentials = () => {
+  return instance.get<ResponseAuth>("/auth/check");
+}
