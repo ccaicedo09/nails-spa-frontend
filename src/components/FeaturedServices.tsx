a711bfbf2742e53279d4a352432f45ce3dcbb96e
Nathalia Avila <nathyavila05@gmail.com>
2025-11-06 15:14:21 -0500

    Refactor service display with ServiceCard component

--- src/components/FeaturedServices.tsx
+++ src/components/FeaturedServices.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
-import { getAllServicesRequest } from '../api/services';
 import { Service } from '../types/servicios';
+import ServiceCard from './ServiceCard';
 
-
-
-const FeaturedServices= ({services}: {services: Service[]}) => {
-  
-  
-
+const FeaturedServices = ({ services }: { services: Service[] }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-      {services && services.map((service, index) => (
-        <div 
-          key={index}
-          className="rounded-2xl p-6 shadow-lg hover:shadow-2xl"
-        >
-          <div className={`w-16 h-16  rounded-full flex items-center justify-center text-3xl mb-4 mx-auto`}>
-            {service.pictureUrl ? (
-              <img src={service.pictureUrl} alt={service.name} className="w-12 h-12 object-cover rounded-full"/>
-            ) : (
-              <span>💅</span>
-            )}
-          </div>
-          <h3 className="text-xl font-bold text-gray-800 mb-2 text-center">
-            {service.name} | {service.duration} min ${service.price}
-          </h3>
-          <p className="text-gray-600 text-sm text-center leading-relaxed">
-            {service.description}
-          </p>
-        </div>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+      {services && services.map((service) => (
+        <ServiceCard key={service._id} service={service} />
       ))}
     </div>
   );
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,6 +3,7 @@ import PDFCard from '../components/PDFCard';
 import Schedule from '../components/Schedule';
 import FeaturedServices from '../components/FeaturedServices';
 import { Service } from '../types/servicios';
+import ServiceCard from '../components/ServiceCard';
 import { getAllServicesRequest } from '../api/services';
 
 const Services = () => {
@@ -24,13 +25,17 @@ const Services = () => {
         <p className="text-gray-600 text-lg">Experiencia y calidad en cada detalle</p>
       </div>
 
-      <div className=" max-w-6xl mx-auto">
-        <FeaturedServices services={services}/>
-        <div className="grid mt-8 grid-cols-1 lg:grid-cols-2 gap-6 ">
-        
+      <div className=" max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        {services && services.map((service) => (
+        <ServiceCard key={service._id} service={service} />
+        ))}
+      </div>
+
+      <div className="grid mt-8 grid-cols-1 lg:grid-cols-2 gap-6 ">  
         <PDFCard services={services}/>
         <Schedule />
-      </div>
+        </div>
       </div>
 
       
